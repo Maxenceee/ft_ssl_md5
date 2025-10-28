b997ed450526055b98a76d0178a53f8ea74300a4
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2025-10-28 13:06:53 +0100

    Add SHA-256 hash function stubs
    Introduces sha2.h and sha2.c with the sh256hash function prototype and a stub implementation. This sets up the structure for SHA-256 hashing functionality, but the actual algorithm is not yet implemented.

---
+++ includes/sha2.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sha2.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 13:05:28 by mgama             #+#    #+#             */
+/*   Updated: 2025/10/28 13:06:44 by mgama            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHA2_H
+#define SHA2_H
+
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define HASH_LENGTH 16 // 128 bits
+
+/**
+ * @brief Construct a new hash object from the given input using the MD5 algorithm.
+ * 
+ * @param input The input data to hash.
+ * @param input_length The length of the input data in bytes.
+ * @param output An optional output buffer to store the resulting hash (16 bytes).
+ * 
+ * @return The function returns a non-zero value on failure.
+ */
+int sh256hash(const uint8_t *input, size_t input_length, uint8_t output[HASH_LENGTH]);
+
+#endif /* SHA2_H */
---
+++ sources/sha2.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sha2.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 13:05:07 by mgama             #+#    #+#             */
+/*   Updated: 2025/10/28 13:06:34 by mgama            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sha2.h"
+
+int
+sh256hash(const uint8_t *input, size_t input_length, uint8_t output[HASH_LENGTH])
+{
+	(void)input;
+	(void)input_length;
+	(void)output;
+	return (1);	
+}
